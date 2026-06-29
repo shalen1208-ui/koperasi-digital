ba025d52f3abd758fe1b29c0843d004f23dc5568
Shal Ven <shalen1208@gmail.com>
2026-06-29 10:39:19 +0700

    fix crash di authroutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,19 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, getProfil } = require('../controllers/authController');
-const verifyToken = require('../middleware/auth');
-
-// POST /api/auth/register
-router.post('/register', register);
-
-// POST /api/auth/login
-router.post('/login', login);
-
-// GET /api/auth/profil
-router.get('/profil', verifyToken, getProfil);
-
-module.exports = router;const express = require('express');
-const router = express.Router();
 const { register, login, getProfil, gantiPassword, lupaPassword, resetPassword, cekToken, requestBantuanPassword } = require('../controllers/authController');
 const verifyToken = require('../middleware/auth');
 
